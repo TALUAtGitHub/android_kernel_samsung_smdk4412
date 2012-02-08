6bff7b43525e0a727ef2cb2e382326680f658db4
Eric W. Biederman <ebiederm@xmission.com>
2012-02-08 07:00:08 -0800

    userns: Store uid and gid values in struct cred with kuid_t and kgid_t types
    cred.h and a few trivial users of struct cred are changed.  The rest of the users
    of struct cred are left for other patches as there are too many changes to make
    in one go and leave the change reviewable.  If the user namespace is disabled and
    CONFIG_UIDGID_STRICT_TYPE_CHECKS are disabled the code will contiue to compile
    and behave correctly.
    
    Change-Id: I71607bf6c2664edea5574872996b25146f352e52
    Acked-by: Serge Hallyn <serge.hallyn@canonical.com>
    Signed-off-by: Eric W. Biederman <ebiederm@xmission.com>

--- include/linux/user_namespace.h
+++ include/linux/user_namespace.h
@@ -56,14 +56,14 @@ static inline void put_user_ns(struct user_namespace *ns)
 }
 
 static inline uid_t user_ns_map_uid(struct user_namespace *to,
-	const struct cred *cred, uid_t uid)
+	const struct cred *cred, kuid_t uid)
 {
-	return uid;
+	return from_kuid_munged(to, uid);
 }
 static inline gid_t user_ns_map_gid(struct user_namespace *to,
-	const struct cred *cred, gid_t gid)
+	const struct cred *cred, kgid_t gid)
 {
-	return gid;
+	return from_kgid_munged(to, gid);
 }
 
 #endif
