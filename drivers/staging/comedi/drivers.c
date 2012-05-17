a42cfb2324c203b60030736534da40b0de559618
H Hartley Sweeten <hartleys@visionengravers.com>
2012-05-16 18:18:52 -0700

    staging: comedi: Add helper macro for comedi usb driver boilerplate
    Introduce the module_comedi_usb_driver macro, and the
    associated register/unregister functions, which is a
    convenience macro for comedi usb driver modules similar
    to module_platform_driver. It is intended to be used by
    drivers where the init/exit section does nothing but
    register/unregister the comedi driver and associated usb
    driver. By using this macro it is possible to eliminate
    a few lines of boilerplate code per comedi usb driver.
    
    Also, when registering the usb driver check for failure
    and unregister the comedi driver.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Cc: Mori Hess <fmhess@users.sourceforge.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers.c
+++ drivers/staging/comedi/drivers.c
@@ -940,3 +940,30 @@ void comedi_usb_auto_unconfig(struct usb_device *usbdev)
 	comedi_auto_unconfig(&usbdev->dev);
 }
 EXPORT_SYMBOL_GPL(comedi_usb_auto_unconfig);
+
+int comedi_usb_driver_register(struct comedi_driver *comedi_driver,
+		struct usb_driver *usb_driver)
+{
+	int ret;
+
+	ret = comedi_driver_register(comedi_driver);
+	if (ret < 0)
+		return ret;
+
+	ret = usb_register(usb_driver);
+	if (ret < 0) {
+		comedi_driver_unregister(comedi_driver);
+		return ret;
+	}
+
+	return 0;
+}
+EXPORT_SYMBOL_GPL(comedi_usb_driver_register);
+
+void comedi_usb_driver_unregister(struct comedi_driver *comedi_driver,
+		struct usb_driver *usb_driver)
+{
+	usb_deregister(usb_driver);
+	comedi_driver_unregister(comedi_driver);
+}
+EXPORT_SYMBOL_GPL(comedi_usb_driver_unregister);
