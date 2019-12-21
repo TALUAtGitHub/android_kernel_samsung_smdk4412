e80f75cb0e52bd03073a13fd89fae971c86c9c1f
TALU <taluemail272@gmail.com>
2019-12-21 13:14:18 +0000

    usb: gadget: restore back to uid_t and gid_t for now.
    Change-Id: If4ea6c33eb33ead44820395d37598af79afba532

--- drivers/usb/gadget/f_fs.c
+++ drivers/usb/gadget/f_fs.c
@@ -218,8 +218,8 @@ struct ffs_data {
 	/* File permissions, written once when fs is mounted */
 	struct ffs_file_perms {
 		umode_t				mode;
-		kuid_t				uid;
-		kgid_t				gid;
+		uid_t				uid;
+		gid_t				gid;
 	}				file_perms;
 
 	/*
@@ -1152,19 +1152,10 @@ static int ffs_fs_parse_opts(struct ffs_sb_fill_data *data, char *opts)
 			break;
 
 		case 3:
-			if (!memcmp(opts, "uid", 3)) {
-				data->perms.uid = make_kuid(current_user_ns(), value);
-				if (!uid_valid(data->perms.uid)) {
-					pr_err("%s: unmapped value: %lu\n", opts, value);
-					return -EINVAL;
-				}
-			}
+			if (!memcmp(opts, "uid", 3))
+				data->perms.uid = value;
 			else if (!memcmp(opts, "gid", 3))
-				data->perms.gid = make_kgid(current_user_ns(), value);
-				if (!gid_valid(data->perms.gid)) {
-					pr_err("%s: unmapped value: %lu\n", opts, value);
-					return -EINVAL;
-				}
+				data->perms.gid = value;
 			else
 				goto invalid;
 			break;
@@ -1193,8 +1184,8 @@ ffs_fs_mount(struct file_system_type *t, int flags,
 	struct ffs_sb_fill_data data = {
 		.perms = {
 			.mode = S_IFREG | 0600,
-			.uid = GLOBAL_ROOT_UID,
-			.gid = GLOBAL_ROOT_GID,
+			.uid = 0,
+			.gid = 0,
 		},
 		.root_mode = S_IFDIR | 0500,
 	};
