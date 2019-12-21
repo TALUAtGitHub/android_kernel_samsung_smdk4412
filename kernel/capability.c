ce001e00721ecc6845c7aff9d5dab3fbf526d915
TALU <taluemail272@gmail.com>
2019-12-21 10:21:33 +0000

    Some things from v3.10.
    Change-Id: Ife7a7608462496f0805b8b4ea608396013909c5b

--- kernel/capability.c
+++ kernel/capability.c
@@ -438,5 +438,6 @@ bool inode_capable(const struct inode *inode, int cap)
 {
 	struct user_namespace *ns = current_user_ns();
 
-	return ns_capable(ns, cap) && kuid_has_mapping(ns, inode->i_uid);
+	 return ns_capable(ns, cap) && kuid_has_mapping(ns, inode->i_uid) &&
+                kgid_has_mapping(ns, inode->i_gid);
 }
