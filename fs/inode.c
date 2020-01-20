eb4436b25d2915b180f23f527b4b5a5d1fc39caf
TALU <taluemail272@gmail.com>
2020-01-20 08:26:29 +0000

    fs: update inode_owner_or_capable.
    Change-Id: I5880997803aa3246ebd0848af08939c116ae3b4e

--- fs/inode.c
+++ fs/inode.c
@@ -1735,9 +1735,13 @@ EXPORT_SYMBOL(inode_init_owner);
  */
 bool inode_owner_or_capable(const struct inode *inode)
 {
+	struct user_namespace *ns;
+
 	if (uid_eq(current_fsuid(), inode->i_uid))
 		return true;
-	if (inode_capable(inode, CAP_FOWNER))
+
+	ns = current_user_ns();
+	if (ns_capable(ns, CAP_FOWNER) && kuid_has_mapping(ns, inode->i_uid))
 		return true;
 	return false;
 }
